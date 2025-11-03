a5b13658d5dc5195b7a6c3b0a8e28a3f4cfae6cf
DongilMin <tasabi0826@gmail.com>
2025-11-03 23:03:17 +0900

    Feat: DynamoDB 채팅방 테이블 구성

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -4,32 +4,17 @@ resource "aws_dynamodb_table" "chat_messages" {
   billing_mode   = var.billing_mode
   read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
   write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
-  hash_key       = "roomId"
+  hash_key       = "pk"
   range_key      = "timestamp"
 
   attribute {
-    name = "roomId"
+    name = "pk"
     type = "S"
   }
 
   attribute {
     name = "timestamp"
     type = "N"
-  }
-
-  attribute {
-    name = "userId"
-    type = "S"
-  }
-
-  # GSI for querying messages by user
-  global_secondary_index {
-    name            = "userId-timestamp-index"
-    hash_key        = "userId"
-    range_key       = "timestamp"
-    projection_type = "ALL"
-    read_capacity   = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
-    write_capacity  = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
   }
 
   ttl {
@@ -47,75 +32,6 @@ resource "aws_dynamodb_table" "chat_messages" {
 
   tags = {
     Name        = "${var.project_name}-${var.environment}-messages"
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
-
-# DynamoDB Table for WebSocket Connections
-resource "aws_dynamodb_table" "connections" {
-  name           = "${var.project_name}-${var.environment}-connections"
-  billing_mode   = var.billing_mode
-  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
-  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
-  hash_key       = "connectionId"
-
-  attribute {
-    name = "connectionId"
-    type = "S"
-  }
-
-  attribute {
-    name = "userId"
-    type = "S"
-  }
-
-  # GSI for querying connections by user
-  global_secondary_index {
-    name            = "userId-index"
-    hash_key        = "userId"
-    projection_type = "ALL"
-    read_capacity   = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
-    write_capacity  = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
-  }
-
-  ttl {
-    attribute_name = "ttl"
-    enabled        = true
-  }
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  server_side_encryption {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "${var.project_name}-${var.environment}-connections"
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
-
-# DynamoDB Table for User Counter (auto-increment user IDs)
-resource "aws_dynamodb_table" "user_counter" {
-  name           = "${var.project_name}-${var.environment}-user-counter"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "counterId"
-
-  attribute {
-    name = "counterId"
-    type = "S"
-  }
-
-  server_side_encryption {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "${var.project_name}-${var.environment}-user-counter"
     Project     = var.project_name
     Environment = var.environment
   }
--- terraform/modules/dynamodb/outputs.tf
+++ terraform/modules/dynamodb/outputs.tf
@@ -7,32 +7,3 @@ output "table_arn" {
   description = "DynamoDB messages table ARN"
   value       = aws_dynamodb_table.chat_messages.arn
 }
-
-output "connections_table_name" {
-  description = "DynamoDB connections table name"
-  value       = aws_dynamodb_table.connections.name
-}
-
-output "connections_table_arn" {
-  description = "DynamoDB connections table ARN"
-  value       = aws_dynamodb_table.connections.arn
-}
-
-output "user_counter_table_name" {
-  description = "DynamoDB user counter table name"
-  value       = aws_dynamodb_table.user_counter.name
-}
-
-output "user_counter_table_arn" {
-  description = "DynamoDB user counter table ARN"
-  value       = aws_dynamodb_table.user_counter.arn
-}
-
-output "all_table_arns" {
-  description = "List of all DynamoDB table ARNs"
-  value = [
-    aws_dynamodb_table.chat_messages.arn,
-    aws_dynamodb_table.connections.arn,
-    aws_dynamodb_table.user_counter.arn
-  ]
-}
